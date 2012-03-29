df88f134019d05975425b342d36934c5079dea4d
Andres Riofrio <riofrios@gmail.com>
2012-03-29 13:57:36 -0700

    Added versioning

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
-app.get('/img', function(req, res) {
+app.get('/v1/img', function(req, res) {
   if(!req.param('url') || req.param('size') == null) {
     res.send('Usage: /img?url=URL&size=IMAGEMAGICK_SIZE', 400);
   } else if(req.param('size') == '') { // empty string is just no resize
