26d85dbd4e1e0b6bcf74b49e94e4a49573b3c663
Andres Riofrio <riofrios@gmail.com>
2012-03-29 13:57:10 -0700

    Initial commit

---
+++ web.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    request = require('request'),
+    im      = require('im');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+app.get('/img', function(req, res) {
+  if(!req.param('url') || req.param('size') == null) {
+    res.send('Usage: /img?url=URL&size=IMAGEMAGICK_SIZE', 400);
+  } else if(req.param('size') == '') { // empty string is just no resize
+    res.redirect(req.param('url')); // 302. TODO: 301 (permanent)?
+  } else {
+    im(request.get(req.param('url'))).resize(req.param('size')).convert(res);
+  }
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
